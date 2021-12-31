5073e0fda845c6f47de871972b3015275090b059
Alonso B <bermejoalonso7@gmail.com>
2021-12-31 10:09:12 +0100

    Cambio ruta SW

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
+const url = window.location.href;
+
+const swLocation = "/twittor/sw.js";
+
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register("../sw.js");
+    if (url.includes("localhost")) {
+        navigator.serviceWorker.register(swLocation);
+    } else {
+        navigator.serviceWorker.register("../sw.js");
+    }
 }
 
 // Referencias de jQuery
