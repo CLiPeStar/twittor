9df83c75dc71a12174626855bdb65acab1cfbebf
Alonso B <bermejoalonso7@gmail.com>
2021-12-31 10:43:46 +0100

    modificaciones de Iphone

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts("js/sw-utils.js");
 
-const STATIC_CACHE = "static-v2";
-const DYNAMIC_CACHE = "dynamic-v1";
+const STATIC_CACHE = "static-v3";
+const DYNAMIC_CACHE = "dynamic-v2";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHELL = ["index.html", "css/style.css", "img/favicon.ico", "img/avatars/hulk.jpg", "img/avatars/ironman.jpg", "img/avatars/spiderman.jpg", "img/avatars/thor.jpg", "img/avatars/wolverine.jpg", "js/app.js", "js/sw-utils.js"];
@@ -19,6 +19,9 @@ self.addEventListener("activate", (e) => {
     const response = caches.keys().then((keys) => {
         keys.forEach((key) => {
             if (key !== STATIC_CACHE && key.includes("static")) {
+                return caches.delete(key);
+            }
+            if (key !== DYNAMIC_CACHE && key.includes("dynamic")) {
                 return caches.delete(key);
             }
         });
